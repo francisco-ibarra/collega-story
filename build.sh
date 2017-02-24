d2b1d0e90bfcdbf90ae45bc9b0310285cf71c31c
Marcos Baez <mbaezpy@gmail.com>
2017-02-24 09:57:09 +0100

    fixed configuration files

--- build.sh
+++ build.sh
@@ -1,7 +1,9 @@
+mkdir public/js
 cp node_modules/axios/dist/axios.min.js public/js/
 cp node_modules/vue/dist/vue.min.js public/js/
 cp node_modules/vue-router/dist/vue-router.min.js public/js/
 cp node_modules/swiper/dist/js/swiper.min.js public/js/
 
+mkdir public/css
 cp node_modules/swiper/dist/css/swiper.min.css public/css/
 cp client/css/style.css public/css/
