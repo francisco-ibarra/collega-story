1e04dd616053bdbfb31532c45c9a1227c684e950
Marcos Baez <mbaezpy@gmail.com>
2017-03-08 16:30:00 +0100

    added api for tagging

--- server/apis.js
+++ server/apis.js
@@ -47,6 +47,37 @@ exports.init = function (app) {
 
   });
 
+  app.post('/api/stories', function (req, res) {
+    var story = req.body;
+
+    console.log(story);
+    res.status(200).send();
+
+  });
+
+  app.post('/api/photos/:id/tags', function (req, res) {
+    var id = req.params.id;
+    var tags = req.body;
+
+    console.log(id);
+    console.log(tags);
+
+    var photo = fnGetPhoto(req.session.user, id);
+    
+    if (!photo) {
+      res.status(404).send();
+      return;
+    }
+    
+    // update tags
+    photo.tags.place = tags.place ? tags.place : photo.tags.place;
+    photo.tags.date = tags.date ? tags.date : photo.tags.place;
+    photo.tags.people = tags.people ? tags.people : photo.tags.place;
+    
+    res.send(photo);
+
+  });
+
   // legacy code
   app.get('/api/user/:id/photos', function (req, res) {
     var id = req.params.id;
@@ -61,12 +92,13 @@ exports.init = function (app) {
         res.send(posts);
       }
     });
-
-
   });
 
 };
 
+/**
+ * Generates demo data from Instagram account
+ */
 var fnLoadPosts = function (id, cb) {
 
   var options = {
@@ -89,6 +121,10 @@ var fnLoadPosts = function (id, cb) {
   });
 };
 
+
+/**
+ * Generates demo data from Instagram account
+ */
 var fnProcessPosts = function (user, posts) {
 
   // accounts the user is managing
@@ -106,15 +142,16 @@ var fnProcessPosts = function (user, posts) {
   // processing posts
   posts.items.forEach(function (item) {
     var photo = {
+      id: item.id,
       images: {
         thumbnail: item.images.thumbnail.url,
         standard: item.images.standard_resolution.url
       },
       contributor: item.user,
       tags: {
-        people: [],
+        people: '',
         date: '',
-        place: item.location
+        place: item.location? item.location.name : null
       },
       stories: [],
       created_time: item.created_time,
@@ -124,6 +161,7 @@ var fnProcessPosts = function (user, posts) {
 
     if (Math.random() > 0.75) {
       var story = {
+        id: 'S' + photo.id,
         photo: photo,
         story: item.caption.text,
         from: item.caption.from,
@@ -158,7 +196,7 @@ var fnProcessPosts = function (user, posts) {
   person.username = profile.username;
   person.full_name = profile.full_name;
   person.profile_picture = profile.profile_picture;
-  
+
   // The user can manage more than one account
   user.accounts.push(person);
 
@@ -175,4 +213,18 @@ var fnGetAccount = function (user, accountId) {
   return user.accounts.find(function (person) {
     return person.id === accountId;
   });
+};
+
+var fnGetPhoto = function (user, photoId) {
+
+  var photo = null;
+  user.accounts.some(function (account) {
+
+    photo = account.photos.find(function (photo) {
+      return photo.id == photoId;
+    });
+
+    return photo != undefined;
+  });
+  return photo;
 };
